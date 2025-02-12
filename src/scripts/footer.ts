4ac0e0bf958a200e29c564296a55aef598dfc94d
Will Gaechter <wgaechter@gmail.com>
2025-02-12 12:14:57 -0500

    projects appearing on page, setting up component foir the cards

--- src/scripts/footer.ts
+++ src/scripts/footer.ts
@@ -5,12 +5,12 @@ $("#swissMode").on('click', function(e) {
     if (LangMode) {
         $("#headerName_text").text("William Joseph Gächter");
         $("#copyright").text("© 2025 - William Gächter - Data Analyst and Engineer")
-        $(this).find('img').attr('src', "/src/assets/US_flag.svg");
+        $(this).find('img').attr('src', "../assets/US_flag.svg");
         LangMode = false;
     } else { 
         $("#headerName_text").text("William Joseph Gaechter");
         $("#copyright").text("© 2025 - William Gaechter - Data Analyst and Engineer");
-        $(this).find('img').attr('src', "/src/assets/Flag_of_Switzerland.svg");
+        $(this).find('img').attr('src', "../assets/Flag_of_Switzerland.svg");
         LangMode = true;
     };
 });
--- src/scripts/projects.ts
+++ src/scripts/projects.ts
@@ -1,4 +1,48 @@
 import $ from 'jquery';
+import { Octokit } from 'octokit';
+
+const octokit = new Octokit();
+
+export async function getRepos(octokit: Octokit) {
+    try {
+        const response = await octokit.request('GET /users/wgaechter/repos', {
+            username: 'wgaechter',
+            type: 'all',
+            sort: 'created',
+            direction: 'desc'
+        });
+        return response.data
+    } catch (error) {
+        console.error('Error fetching repos:', error);
+        return null;
+    };
+};
+
+export function displayRepos() {
+    var data = getRepos(octokit);
+    if (data == null) {
+        console.log("No data found");
+        return '<p>No project data found.</p>';
+    }
+
+    data.then((repos) => {
+        console.log(repos);
+        for (let repo of repos) {
+            const cardHtml = `
+            <div class="card" id="${repo.name}">
+                <div class="card-body">
+                <h5 class="card-title">${repo.name}</h5>
+                <p class="card-text">${repo.description}</p>
+                <a href="${repo.html_url}" class="btn btn-primary">Go to Repo</a>
+                </div>
+            </div>`;
+            $('#repo-container').append(cardHtml);
+        }
+        });
+    //For each repo, create a card
+}
+
+displayRepos();
 
 //call github api
 //get all repos
