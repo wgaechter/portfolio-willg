0c55e4ecae4df5011c4577934205e1e2f51c5190
Will Gaechter <wgaechter@gmail.com>
2025-02-18 12:53:51 -0500

    Updated markdown cards, working kinda

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,2 +1,14 @@
 import $ from 'jquery';
 
+//on click of md container grid
+$("md-card").on('click', function(this: HTMLElement) {
+    console.log('clicked');
+});
+    // change id/class of clicked element to active
+    // fade all inactive elements
+    // transform active element to full screen and adjust
+
+//on return to grid:
+    //set active element to inactive
+    //fade full screen out
+    //fade grid back in one by one
